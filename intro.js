ebb2395af70cf7ec549be0985b9711e77dc90c12
Manikanta Prasad Lopinti <manikantatez@gmail.com>
2025-02-09 17:11:09 +0530

    redis intro

---
+++ intro.js
@@ -0,0 +1,15 @@
+/*
+    Redis is a in-memory database (writes in disk in batches to persists the data that we need)
+    Redis is no-sql key-value data store
+
+    offers these types of softwares
+        1. redis community  (open source software)
+        2. redis stack (open source with modules added)
+        3. redis Enterprice software (self manage)
+            1. redis modules
+            2. supports multi region sahrding and replication 
+            3. redis on flash 
+        2. redis cloud (enterprise auto manage)
+
+    
+*/
